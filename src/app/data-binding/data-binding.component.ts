5476efbe2d0ab0f005d213e5d895d6c2ce7eb078
Bismarck Renan <bismarck@forlogic.net>
2019-10-03 14:15:42 -0300

    event binding and using bootstrap

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,12 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
-  url: string = "http.biu.net.br";
+  url: string = 'http.biu.net.br';
   cursoAngular: boolean = true;
-  urlImagem: string = "http://lorempixel.com/400/200/nature/"
+  urlImagem: string = 'http://lorempixel.com/400/200/nature/';
+  actualValue: string = '';
+  savedValue: string = '';
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1+1;
+  }
+
+  clickButton(){
+    alert('YOU clicked in this Button')
+  }
+
+  saveValue(value){
+    this.savedValue = value;
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement> event.target).value)
+    this.actualValue = (event.target as HTMLInputElement).value
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
